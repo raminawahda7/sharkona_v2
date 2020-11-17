99c47f486bd1a23585a9b7332254c6870848d8d7
raminawahda <rami.nawahda@gmail.com>
2020-11-17 20:19:45 +0200

    solving conflicts for frontEnd

---
+++ front/src/components/Home/event/event.js
@@ -0,0 +1,21 @@
+import React , {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ const Event = (props) => {
+        return (
+            <section className="bg-light page-section" id="event">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading text-uppercase">Events</h2>
+              <h3 className="section-subheading text-muted"> The place of magic .</h3>
+            </div>
+          </div>
+          <div className="row">
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Event
